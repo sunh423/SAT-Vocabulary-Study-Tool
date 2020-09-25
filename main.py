b61db731f79c42f0564fbc15429d8d618208297c
sunh423 <64500766+sunh423@users.noreply.github.com>
2020-09-24 22:43:54 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 from bs4 import BeautifulSoup
+from random import choice, shuffle
 import requests
 import csv
-import os
-
-def clear():
-    os.system('cls' if os.name=='nt' else 'clear')
 
 version = "v1.0"
 
 class VocabGame:
 	GAME_MODES = {"1": "Standard"}
 	GROUPING_CHOICES = {"1":"All", "2":"Nouns Only", "3":"Verbs Only", "4":"Adjectives Only"}
-	USER_CHOICES_1 = {"start", "options"}
+	USER_CHOICES_1 = {"start", "options", "update"}
 
-	def __init__(self, mode=1, grouping="All"):
-		print(f"Welcome to the SAT Vocabulary Study Tool {version} created by Howard Sun.")
-		print("This program will allow you to efficiently review and learn quintessential SAT words needed for success.")
+	def __init__(self, mode="Standard", grouping="All"):
 		self.vocab = self._get_vocabs()
 		self.mode = mode
 		self.grouping = grouping
+		self.streak = 0
+
+		print(f"Welcome to the SAT Vocabulary Study Tool {version} created by Howard Sun.")
+		print("This program will allow you to efficiently review and learn quintessential SAT words needed for success.")
 		self._menu()
 
 	def _menu(self):
@@ -31,8 +30,10 @@ class VocabGame:
 			self._game_start()
 		elif choice_1 == "options":
 			self._options()
-		say_hi()
-		clear()
+		elif choice_1 == "update":
+			self._update_vocabs()
+			print("Update complete!")
+			self._menu()
 		
 
 	def _options(self):
@@ -53,15 +54,56 @@ class VocabGame:
 			self.grouping = VocabGame.GROUPING_CHOICES[choice_3]
 			print(f"'{self.grouping}' has been selected.")
 		self._menu()
-		say_hi()
-		clear()
 
 
 	def _vocab_reset(self):
 		self.vocab = self._get_vocabs()
 
 	def _game_start(self):
-		pass
+		if self.mode == "Standard":
+			if self.grouping == "All":
+				temp_list = []
+				temp_request = choice(list(self.vocab.keys()))
+				temp_list.append(self.vocab[temp_request][0])
+
+				while len(temp_list) < 4:
+					appending = self.vocab[choice(list(self.vocab.keys()))][0]
+					if appending not in temp_list:
+						temp_list.append(appending)
+				shuffle(temp_list)
+				choices = dict(enumerate(temp_list,1))
+				
+				print(f"Which of the following is the definition of {temp_request}?")
+				for i, value in choices.items():
+					print(f"{i} - {value}")
+				response = input("Pick the number corresponding to your choice: ")
+				while response not in {'1','2','3','4'}:
+					response = input("You must enter a number out of the 4 corresponding options: ")
+				response = int(response)
+
+				if choices[response] == self.vocab[temp_request][0]:
+					self.streak += 1
+					print(f"You are correct!")
+					if self.streak > 2:
+						print(f"You are on a streak of {self.streak}. Amazing!")
+				else:
+					self.streak = 0
+					print(f"Incorrect! The correction definition for {temp_request} is: {self.vocab[temp_request][0]}.")
+
+				temp_list.clear()
+
+				again = input("Would you like to continue? (Y/N)")
+				if not again:
+					again = "y"
+				while again[0].lower() not in {"y","n"}:
+					again = input("Would you like to continue? (Y/N)")
+
+				if again[0].lower() == "y":
+					self._game_start()
+				elif again[0].lower() == "n":
+					self._menu()
+		else:
+			pass
 
 	def _get_vocabs(self):
 		with open("vocabs.csv") as file:
@@ -86,11 +128,10 @@ class VocabGame:
 					continue
 
 				index = tag.contents[1].get_text()
-				vocab =  tag.contents[3].get_text().title()
-				definition = tag.contents[5].get_text().title()
+				vocab =  tag.contents[3].get_text().title().strip()
+				definition = tag.contents[5].get_text().title().strip()
 				grouping = tag.contents[7].get_text().title()
 
 				writer.writerow({"Index":index,"Word":vocab, "Definition":definition, "Grouping":grouping})
 
-clear()
 VocabGame()
